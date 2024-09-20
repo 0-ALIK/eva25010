4eb010372837fbb9f70975b43dbeafe267635b97
AnghelKupo <shawtybrave.lexus@gmail.com>
2024-09-20 18:06:19 -0500

    variables de diseño

--- src/pages/gestion-usuario/Register.vue
+++ src/pages/gestion-usuario/Register.vue
@@ -1,9 +1,66 @@
 <template>
-    <div>
-        <h2>Registro</h2>
+    <div class="h-screen w-screen overflow-hidden bg-black bg-main bg-cover bg-no-repeat">
+        <div class="m-outer bg-gray-500">
+            <h1 class="p-3 m-5">Registro</h1>
+
+            <section class="flex flex-wrap ">
+
+                <article>
+                    <label for="username">Username</label>
+                    <div class="flex flex-col gap-2">
+                        <IconField>
+                            <InputIcon class="pi pi-user text-primary-500" style="color: var(--p-Sprimary-500)" />
+                            <InputText  placeholder="Search" type="text" />
+                        </IconField>
+                    </div>
+                </article>
+
+                <article>
+                    <label for="username">Username</label>
+                    <div class="flex flex-col gap-2">
+                        <IconField>
+                            <InputIcon class="pi pi-user text-primary-500" style="color: var(--p-Sprimary-500)" />
+                            <InputText  placeholder="Search" type="text" />
+                        </IconField>
+                    </div>
+                </article>
+
+                <article>
+                    <label for="username">Username</label>
+                    <div class="flex flex-col gap-2">
+                        <IconField>
+                            <InputIcon class="pi pi-user text-primary-500" style="color: var(--p-Sprimary-500)" />
+                            <InputText  placeholder="Search" type="text" />
+                        </IconField>
+                    </div>
+                </article>
+
+                <article>
+                    <label for="username">Username</label>
+                    <div class="flex flex-col gap-2">
+                        <IconField>
+                            <InputIcon class="pi pi-user text-primary-500" style="color: var(--p-Sprimary-500)" />
+                            <InputText  placeholder="Search" type="text" />
+                        </IconField>
+                    </div>
+                </article>
+            </section>
+
+            <section>
+
+                
+
+            </section>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import InputText from 'primevue/inputtext';
+
+
+
 
 </script>
