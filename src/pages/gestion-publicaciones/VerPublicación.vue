914a68ed559a09fb49f446d7cc7a8b3e5d3d7db1
Alik <moisesalik01@gmail.com>
2024-10-01 17:33:31 -0500

    xd

--- src/pages/gestion-publicaciones/VerPublicación.vue
+++ src/pages/gestion-publicaciones/VerPublicación.vue
@@ -9,17 +9,31 @@
 
         <div class="flex gap-4">
             
-            <div class="p-5 rounded-md bg-surface-900 w-full">
+            <div class="p-5 rounded-md bg-surface-900 w-full flex flex-col justify-between">
                 <section>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ipsum architecto alias nemo eos velit molestias. Autem eveniet odit culpa, ducimus provident explicabo, amet aut id repudiandae eius delectus sit veritatis aperiam velit mollitia rerum et aliquam obcaecati dolorum quia ipsa eligendi cumque fuga. Est unde distinctio non doloremque assumenda?</p>
                 </section>
 
                 <section>
-                    
+
+                    <div>
+                        <div class="flex items-center bg-surface-800 p-2 rounded-md w-fit">
+                            <Avatar icon="pi pi-user" class="mr-2" size="large" style="background-color: #ece9fc; color: #2a1261" shape="circle" />
+                            <div>
+                                <p>Nombre Apellido</p>
+                                <small class="text-gray-500">Ocupación</small>
+                            </div>
+                        </div>
+
+                        <div>
+                            
+                        </div>
+                    </div>
+
                 </section>
             </div>
 
-            <div class="w-1/3">
+            <div class="w-3/5">
                 <Image src="https://cdn.pixabay.com/photo/2023/10/29/14/37/pumpkins-8350480_1280.jpg" alt="Image" width="500" preview />
             </div>
 
@@ -30,4 +44,5 @@
 <script setup lang="ts">
 import Chip from 'primevue/chip';
 import Image from 'primevue/image';
+import Avatar from 'primevue/avatar';
 </script>
